1f44a293db1eea83a76d7e5562bc0740ef5912bd
LeoRiether <leonardoalves8217@gmail.com>
2021-05-15 18:21:39 -0300

    Explain channels

--- src/main/scala/channel/channel.scala
+++ src/main/scala/channel/channel.scala
@@ -2,10 +2,14 @@ package channel
 
 import scala.collection.mutable.Map
 
+/**
+ * A Channel[M] can publish messages of type M subscribers/handlers of type M => Unit
+ */
 class Channel[M] {
     var subscribers = Map.empty[Int, M => Unit]
     var nextID = 0
 
+    /** Subscribes a handler and returns a function to unsubscribe from the channel */
     def subscribe(handler: M => Unit) : () => Unit = {
         val id = nextID
         nextID += 1
@@ -17,6 +21,8 @@ class Channel[M] {
     def publish(message: M): Unit =
         subscribers.foreach(s => s._2(message))
 
+    // The arrows indicate data flow. Data goes from the channel to the handler,
+    // and from the message to the channel.
     def >>(handler: M => Unit) = subscribe(handler)
     def <<(message: M) = publish(message)
 }
